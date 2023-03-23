df0ba467d6b3d74bc5024e89a2b787716fda0b6f
Rincy <rincyaslp@gmail.com>
2023-03-23 16:25:55 -0500

    GA4 Completed

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,34 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Size(min=3)
+    @NotBlank
+    private String location;
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs=new ArrayList<Job>();
 
+    public Employer(){
+
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
